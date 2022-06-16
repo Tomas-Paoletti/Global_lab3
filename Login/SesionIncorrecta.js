1b24ddc692439442e53b7db2b6a3ff3425159a38
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2022-06-16 01:39:53 -0300

    sql

--- Login/SesionIncorrecta.js
+++ Login/SesionIncorrecta.js
@@ -1,3 +1,18 @@
-let message = "Error Usuario o contraseña invalidos";
-
-alert(message);
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('usuario').value;
+    if(usuario.length == 0) {
+      alert('No has escrito nada en el usuario');
+      return;
+    }
+    var clave = document.getElementById('clave').value;
+    if (clave.length < 6) {
+      alert('La clave no es válida');
+      return;
+    }
+    this.submit();
+  }
